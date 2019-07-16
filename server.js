4ba4d9c3f90c7bd526c9066756cb7bfb6c4c6f95
Phil Dugliss <phillipdugliss@gmail.com>
2019-07-16 15:52:33 -0500

    Database connected
    -Models Created
    -Schemas created
    -Database Connected
    -Packages initialized

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+//Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//DB Connection
+mongoose.connect("mongodb://localhost/NYTScraper", { useNewUrlParser: true });
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/NYTScraper";
+
+// mongoose.connect(MONGODB_URI);
+
+//Routes
+
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://www.nytimes.com/?login=google").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text("");
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+
+
+
+
+
+//Server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
